7727988655390625700ba6bc27e67c116ba889f2
Weining Liu <liuweining@gmail.com>
2013-06-10 10:36:09 -0400

    deploy script no long copy lucene libs, all jars are copied to versions.alt;test execution script classpath fixed;tested working with t1;jacoco.xml is not yet fixed

---
+++ TestScripts/testClasspath.sh~
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+TESTSUBJECT="apache_solr_core_snapshots-TM"
+TESTSUBJECT_ALT="apache_solr_core_snapshots"
+VER=v0
+CLASS_DIR="/home/wliu/sir/$TESTSUBJECT/versions.alt/orig/$VER/$TESTSUBJECT_ALT/build/classes"
+
+ls .
+ls $CLASS_DIR/solr-core/classes/test
+ls $CLASS_DIR/solr-core/classes/java
+ls $CLASS_DIR/solr-core/test
+ls $CLASS_DIR/solr-core/test-files
+ls /home/wliu/sir/$TESTSUBJECT/versions.alt/orig/$VER/$TESTSUBJECT_ALT/lib/*
--- deploy_snapshots.sh
+++ deploy_snapshots.sh
@@ -66,37 +66,6 @@ do
         cd ..
         rm -rf lucene
         svn checkout http://svn.apache.org/repos/asf/lucene/dev/trunk/lucene -r $TIMESTAMP
-cd lucene
-ant package
-cd build
-mkdir $HOME/sir/workspace/apache-solr/lucenelib
-cp ./grouping/lucene-grouping-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./join/lucene-join-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./benchmark/lucene-benchmark-5.0-SNAPSHOT-javadoc.jar ../../lucenelib
-cp ./demo/lucene-demo-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./classification/lucene-classification-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./misc/lucene-misc-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./sandbox/lucene-sandbox-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/phonetic/lucene-analyzers-phonetic-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/icu/lucene-analyzers-icu-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/stempel/lucene-analyzers-stempel-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/common/lucene-analyzers-common-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/uima/lucene-analyzers-uima-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/morfologik/lucene-analyzers-morfologik-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/smartcn/lucene-analyzers-smartcn-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./analysis/kuromoji/lucene-analyzers-kuromoji-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./core/lucene-core-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./memory/lucene-memory-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./test-framework/lucene-test-framework-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./queryparser/lucene-queryparser-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./suggest/lucene-suggest-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./highlighter/lucene-highlighter-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./queries/lucene-queries-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./facet/lucene-facet-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./spatial/lucene-spatial-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./replicator/lucene-replicator-5.0-SNAPSHOT.jar ../../lucenelib
-cp ./codecs/lucene-codecs-5.0-SNAPSHOT.jar ../../lucenelib
-cd ../..
 	cd trunk-$VER
 
 	echo " 4 === build" 
@@ -125,23 +94,42 @@ cd ../..
 	echo " copy source - only copy source of core"
 	cp -r core $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/build/src
  
-        echo "copy libs "
-	cp -r ./core/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./core/src/test-files/solr/collection1/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./test-framework/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./solrj/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/langid/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/analysis-extras/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/uima/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/extraction/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/velocity/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/dataimporthandler/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-	cp -r ./contrib/clustering/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
-
-	echo " move lucenelib "
-	mv ../lucenelib $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT
-
-        echo "copy test files"
+	echo " copy libs"
+	cp ./build/lucene-libs/*.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/solr-test-framework/lucene-libs/lucene-test-framework-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/solr-test-framework/solr-test-framework-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/solr-core/solr-core-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-cell/solr-cell-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-dataimporthandler/solr-dataimporthandler-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-clustering/solr-clustering-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-langid/solr-langid-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-velocity/solr-velocity-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-dataimporthandler-extras/solr-dataimporthandler-extras-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-uima/lucene-libs/lucene-analyzers-uima-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-uima/solr-uima-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-analysis-extras/lucene-libs/lucene-analyzers-smartcn-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-analysis-extras/lucene-libs/lucene-analyzers-stempel-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-analysis-extras/lucene-libs/lucene-analyzers-icu-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-analysis-extras/lucene-libs/lucene-analyzers-morfologik-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/contrib/solr-analysis-extras/solr-analysis-extras-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./build/solr-solrj/solr-solrj-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solrj-lib/*.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/test-framework/lucene-libs/lucene-test-framework-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/test-framework/lib/junit4-ant-2.0.10.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/test-framework/lib/randomizedtesting-runner-2.0.10.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/test-framework/lib/junit-4.10.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/test-framework/lib/ant-1.8.2.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-dataimporthandler-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-cell-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-langid-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-analysis-extras-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-dataimporthandler-extras-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-velocity-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-solrj-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-test-framework-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-clustering-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./dist/solr-uima-5.0-SNAPSHOT.jar $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
+	cp ./core/lib/* $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b/$TESTSUBJECT_ALT/lib
 
 	cd ..
 
