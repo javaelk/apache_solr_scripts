3fab0ad88c580c82fe39393ddb247a4b71b8c0bc
Weining Liu <liuweining@gmail.com>
2014-01-20 11:47:45 -0500

    change the level of check out from svn. Now always checks out the entire trunk folder which includes lucene and solr subfolders.

--- deploy_snapshots.sh
+++ deploy_snapshots.sh
@@ -2,8 +2,6 @@
 TESTSUBJECT="apache-solr-core-snapshotsAll-TM"
 TESTSUBJECT_ALT="apache-solr-core-snapshots"
 SVNLOCAL="/media/svn/svn-apache"
-SVNNAME="solr"
-SVNCHECKOUT=true #set to false when no connection to SVN
 ENABLETEST=false #set to false to skip ant test- takes long time and many failed
 LOGLOCATION="$HOME/sir/workspace/log"
 java1_5=$HOME/java/jdk1.5.0_22
@@ -25,19 +23,13 @@ echo "1 === fetch from SVN"
 
 cd $SVNLOCAL
 
-if $SVNCHECKOUT; then
+svn checkout -q http://svn.apache.org/repos/asf/lucene/dev/trunk
 
-	svn checkout -q http://svn.apache.org/repos/asf/lucene/dev/trunk/solr
-	svn checkout -q http://svn.apache.org/repos/asf/lucene/dev/trunk/lucene
+cd trunk/solr
+echo "2 === extract version numbers "
 
-	cd $SVNNAME
-	echo "2 === extract version numbers "
-
-	#remove --limit 5 later to see all.  -r 1487914:1491470 
-	svn log --limit 10 | grep "^r[0-9]* |" | sed 's/^r\([0-9]*\).*$/\1/' > ../snapshot_build_hashcode.txt
-	cd ..
-
-fi
+#remove --limit 5 later to see all.  -r 1487914:1491470 
+svn log --limit 1000 | grep "^r[0-9]* |" | sed 's/^r\([0-9]*\).*$/\1/' > ../../snapshot_build_hashcode.txt
 
 cd $SVNLOCAL   
 #b is total number of versions
@@ -47,18 +39,16 @@ LASTVERSION=0  #this points to the previous version number
 #iterate for each version   
 while read VER
 do
-	echo "3 === check out version $VER"
-	if $SVNCHECKOUT; then
-		svn checkout -q http://svn.apache.org/repos/asf/lucene/dev/trunk/solr -r $VER trunk-$VER
-	fi
-
-   	echo "4 === beautify" 
-        ~/sir/workspace/astyle/build/gcc/bin/astyle --style=allman --recursive --suffix=none --quiet trunk-$VER/core/*.java     
+   echo "3 === check out version $VER"
+   svn checkout -q http://svn.apache.org/repos/asf/lucene/dev/trunk -r $VER trunk-$VER
+   
+   echo "4 === beautify" 
+   ~/sir/workspace/astyle/build/gcc/bin/astyle --style=allman --recursive --suffix=none --quiet trunk-$VER/solr/core/*.java     
 
 	if [ "$LASTVERSION" != 0 ]
              then 
              echo "5.1 === compare version $LASTVERSION with $VER"
-		diff_result=` diff --ignore-case --ignore-all-space --ignore-blank-lines --recursive --brief --exclude='.svn' trunk-$LASTVERSION/core/src/java trunk-$VER/core/src/java `
+		diff_result=` diff --ignore-case --ignore-all-space --ignore-blank-lines --recursive --brief --exclude='.svn' trunk-$LASTVERSION/solr/core/src/java trunk-$VER/solr/core/src/java `
 
 		if [ -z "$diff_result" ]
                      then  #empty means they are the same  
@@ -69,23 +59,9 @@ do
 	
         #this VER should be different from LASTVERSION
 	let " b -=1 "
-	cd trunk-$VER
-
-#checkout a Lucene code match the timestamp of solr revision
-#svn checkout http://svn.apache.org/repos/asf/lucene/dev/trunk/lucene -r {2013-06-07T20:53-0400}
-        echo "5.5 === parse TIMESTAMP of version $VER"
-        echo "TIMESTAMP `svn log -r $VER`"
-	TIMESTAMP=`svn log -r $VER | grep "$VER" | sed 's/^r[0-9]* | [a-zA-Z]* | \(.*\)(.*) | .*$/\1/' |sed 's/\([0-9-]*\) \([0-9]*:[0-9]*\):[0-9]* \(.*\) $/{\1T\2\3}/' `
-        echo "TIMESTAMP of version $VER is $TIMESTAMP"
-
-        cd ..
-        rm -rf lucene
-        svn checkout -q http://svn.apache.org/repos/asf/lucene/dev/trunk/lucene -r $TIMESTAMP
-	cd trunk-$VER
-
+	cd trunk-$VER/solr
 
 	echo " 6 === build" 
-	if $SVNCHECKOUT; then
         
         #each major revisions and use different java versions.
 	if [ $VER -ge 1457734 ]
@@ -144,7 +120,6 @@ do
                exit
          fi
          echo "ant dist $VER $build_result" 
-	fi
 
 	echo "7 === deploy r$VER to versions.alt directory as v$b"
 	mkdir -p $HOME/sir/$TESTSUBJECT/versions.alt/orig/v$b
@@ -163,7 +138,7 @@ do
 
 	echo " libs will not be copied. test scripts are run directly in SVN local directories"
 
-	cd ..
+	cd $SVNLOCAL
 
 #TODO: ideally, generate test plan, run test scripts right here and run it in the loop.
 	LASTVERSION=$VER
